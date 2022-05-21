0399cc2b70851f73fcff4b6f20e3558d38c743f3
hiba <hibzrashed@gmail.com>
2022-05-21 13:22:48 -0500

    commit tree a88dc65ae8701fed44308591b8b92c081b6d0cd1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+saturdayFun();
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun('bathe my dog');
+
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+//const highlightFunction = wrapAdjective(result = "*")
+function wrapAdjective(result = "") {
+    const innerFunction = function (adjective = "a hard worker") {
+        return `You are ${result}${adjective}${result}!`;
+}
+return innerFunction
+}
+
+//wrapAdjective();
+//wrapAdjective("||");
+innerFunction();
+
+     
